3d5bc95f781b3e8cd0e44429d0b6cdfa3702a8dc
joric <joric@users.noreply.github.com>
2026-07-16 04:17:18 +0500

    updated files

--- scripts/backport_pz.py
+++ scripts/backport_pz.py
@@ -18,7 +18,7 @@ s = re.sub(r'^section\s+([\w.]+)$', r'(level \1)', s, flags=re.MULTILINE)
 # Add "[ action Player ] -> [ action Player CharacterSwap ]" after RULES block
 s = re.sub(
     r'(^={3,}\s*RULES\s*={3,}\s*\n)',
-    r'\1[ action Player ] -> [ action Player CharacterSwap ]\n\n',
+    r'\1[ action Player ] -> [ action Player CharacterSwap ] (comment that out for puzzlescript-next)\n\n',
     s,
     flags=re.MULTILINE
 )
